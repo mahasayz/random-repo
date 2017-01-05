aff40707a92ff16b8d53e48ca8eaf6dbd1227c4a
Alam, Mahbub (Agoda) <mahbub.alam@agoda.com>
2017-01-05 12:16:52 +0700

    Added CSV Parser to convert to case classes

--- src/main/scala/com/lunatech/interviews/model/Records.scala
+++ src/main/scala/com/lunatech/interviews/model/Records.scala
@@ -5,3 +5,50 @@ package com.lunatech.interviews.model
   */
 
 case class Record(list: List[String] = List("Bangladesh"))
+
+case class Airport(id: Option[Long],
+                   ident: String,
+                   `type`: String,
+                   name: String,
+                   latitudeDeg: Option[Double],
+                   longitudeDeg: Option[Double],
+                   elevationFt: Option[Int],
+                   continent: Option[String],
+                   isoCountry: Option[String],
+                   isoRegion: Option[String],
+                   municipality: Option[String],
+                   scheduledService: Option[String],
+                   gpsCode: Option[String],
+                   iataCode: Option[String],
+                   localCode: Option[String],
+                   homeLink: Option[String],
+                   wikipediaLink: Option[String],
+                   keywords: Option[String])
+
+case class Runway(id: Option[Long],
+                  airportRef: Int,
+                  airportIdent: String,
+                  lengthFt: Int,
+                  widthFt: Int,
+                  surface: String,
+                  lighted: Boolean,
+                  closed: Boolean,
+                  leIdent: Option[String],
+                  leLatitudeDeg: Option[Double],
+                  leLongitudeDeg: Option[Double],
+                  leElevationFt: Option[Int],
+                  leHeadingDegT: Option[Double],
+                  leDisplacedThresholdFt: Option[Int],
+                  heIdent: Option[String],
+                  heLatitudeDeg: Option[Double],
+                  heLongitudeDeg: Option[Double],
+                  heElevationFt: Option[Int],
+                  heHeadingDegT: Option[Double],
+                  heDisplacedThresholdFt: Option[Int])
+
+case class Country(id: Int,
+                   code: String,
+                   name: String,
+                   continent: String,
+                   wikipediaLink: String,
+                   keywords: String)
--- src/test/scala/CsvSpec.scala
+++ src/test/scala/CsvSpec.scala
@@ -1,27 +1,21 @@
+import com.lunatech.interviews.model.Country
+import com.lunatech.interviews.web.CSVConverter
+import com.lunatech.interviews.web.CSVConverter._
 import org.scalatest.{FunSuite, Matchers}
+
+import scala.util.Try
 
 /**
   * Created by malam on 12/22/16.
   */
 class CsvSpec extends FunSuite with Matchers {
 
-  case class Airport(id: Int, ident: String, `type`: String)
-
-  def toCaseClass[A, B](f: B => A)(columns: B): A = {
-    f(columns)
-  }
-
   test("Testing csv readability") {
-    val url = getClass.getClassLoader.getResource("airports.csv")
+    val url = getClass.getClassLoader.getResource("countries.csv")
     val bufferedSource = io.Source.fromFile(url.getPath)
-    for (line <- bufferedSource.getLines().drop(1)) {
-      val cols = line.split(",").map(_.trim)
+    val airports = CSVConverter[List[Country]].from(bufferedSource.getLines().drop(1).mkString("\n"))
 
-      val res = toCaseClass { columns: Array[String] =>
-        Airport(cols(0).toInt, cols(1), cols(2))
-      }(cols)
-      res
-    }
+    airports.isSuccess shouldEqual(true)
   }
 
 }
